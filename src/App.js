b03dec99a8d22aafddc0e46745b1cbadc6f27bcc
Reient <4reient4@gmail.com>
2021-01-02 10:47:42 +0200

    task complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Publication from "./publications.js"
 
+const ANAKIN_IMAGE = <img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg" alt="avatar"/>;
+const RAY_IMAGE = <img src="https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale" alt="post"/>;
+const GROGU_IMAGE = <img src="https://s13.pikabu.ru/post_img/2020/12/19/10/1608400469127029114.jpg" alt="post"/>;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <Publication avatar={ANAKIN_IMAGE} name={"Anakin Skywalker"} nickname="@dart_vader" date={"· 26 лют."}
+      content="WTF? Who is Ray? Why she is Skywalker? Luke...?" image={RAY_IMAGE}
+      />
+      <Publication avatar={ANAKIN_IMAGE} name={"Anakin Skywalker"} nickname="@dart_vader" date={"· 27 лют."}
+      content="WTF? Who is Grogu? Why he is Jedi? Luke...?" image={GROGU_IMAGE}
+      />
     </div>
   );
 }
